54aa231e23b72bbda0c3db946c375f4e7cf0aefb
ranasalameh-63 <rnasalameh@gmail.com>
2024-12-26 23:38:23 +0300

    task

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Javascript-basic
+This task is a simple introduction to JavaScript, ##covering fundamental concepts such as alertbox, prompot box## 
---
+++ index.js
@@ -0,0 +1,47 @@
+// Question 1
+console.log(-5*3)//-15
+console.log("javaScript"+50) // javascript50
+console.log(17%5) // 2
+console.log(5%17) // 5
+console.log(5/10)// 0.5
+console.log(4==='4') // false
+console.log(4!=5) // true
+console.log(7<=8) // true
+console.log("Hello"+5) // Hello5
+let x=5.6;
+console.log(Math.ceil(x)-Math.floor(x)) // 1
+console.log( Math.pow(x,2)) // 1.3599999999999
+
+// Question 2
+let userInput = parseInt(prompt("Enter an integer number"));
+alert('You Entered'+userInput);
+
+// Question 3
+let userIn = parseInt(prompt("Enter a first number"));
+let userInp = parseInt(prompt("Enter a second number"));
+alert('Sum = '+(userIn+userInp));
+
+
+// Question 4
+let userNumber1 = parseInt(prompt("Enter a first number"));
+let userNumber2 = parseInt(prompt("Enter a second number"));
+let product = userNumber1*userNumber2;
+alert("Product = "+product);
+
+// Question 5
+let userNumber11 = parseInt(prompt("Enter a first number"));
+let userNumber22 = parseInt(prompt("Enter a second number"));
+let result = userNumber11/userNumber22;
+alert("Result = "+result);
+
+// Question 6
+let r =4;
+let y =9;
+let summation = r+y;
+alert("Summation = "+summation);
+
+
+
+
+
+
